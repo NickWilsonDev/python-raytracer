787ff5dc95aa0723fa8b66e9e46bc58e3da50a42
Nick Wilson <logikfx@gmail.com>
2025-05-05 18:20:44 -0400

    Added overloaded subtraction operator.

--- Vector.py
+++ Vector.py
@@ -19,6 +19,11 @@ class Vector:
         self.z = self.z + vec.z
 
 
+    def __sub__(self, vec):
+        self.x = self.x - vec.x
+        self.y = self.y - vec.y
+        self.z = self.z - vec.z
+
 
     def __eq__(self, vec):
         if self.x == vec.x and self.y == vec.y and self.z == vec.z:
